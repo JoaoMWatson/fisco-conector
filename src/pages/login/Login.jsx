99713b280abb24cccfd17935dd87d511a7f59cc1
João Pedro Martins Watson <joaopedrowatson@hotmail.com>
2020-09-22 11:17:10 -0300

    add image,

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -9,9 +9,13 @@ export default function Login() {
     <div className="login-container">
       <div class="grid-login">
         <div class="imagem-conector">
-          <img className="image-login" src='../../images/login-image.png' />
+          <img
+            className="image-login"
+            src="https://raw.githubusercontent.com/JoaoMWatson/fisco-conector/5820bb22f9b59f5604a0bd8281aab5ec8c9d8a15/src/images/login-image.png"
+          />
         </div>
         <div class="login-form">
+          <h1 className="login-title">Login</h1>
           <LoginForm />
         </div>
       </div>
