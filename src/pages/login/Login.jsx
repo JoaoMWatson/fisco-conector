5820bb22f9b59f5604a0bd8281aab5ec8c9d8a15
João Pedro Martins Watson <joaopedrowatson@hotmail.com>
2020-09-22 10:48:24 -0300

    adicionando imagem

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -2,12 +2,19 @@ import React from "react";
 import "./Login.css";
 
 import LoginForm from "../../components/loginForm/LoginForm";
-import Footer from "../../components/footer/Footer"
+import Footer from "../../components/footer/Footer";
 
 export default function Login() {
   return (
     <div className="login-container">
-      <LoginForm />
+      <div class="grid-login">
+        <div class="imagem-conector">
+          <img className="image-login" src='../../images/login-image.png' />
+        </div>
+        <div class="login-form">
+          <LoginForm />
+        </div>
+      </div>
       <Footer />
     </div>
   );
