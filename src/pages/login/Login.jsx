691b8a81df825db5eed6511a5c1c9ab7fcc58697
João Pedro Martins Watson <joaopedrowatson@hotmail.com>
2020-09-22 16:49:06 -0300

    melhora no layout

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,14 +1,14 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./Login.css";
 
-import isAuthenticated from "../../auth"
+import isAuthenticated from "../../auth";
 import LoginForm from "../../components/loginForm/LoginForm";
 import Footer from "../../components/footer/Footer";
 
 export default function Login(props) {
   const [callbackAuth, setCallbackAuth] = useState("");
 
-  isAuthenticated(callbackAuth)
+  isAuthenticated(callbackAuth);
 
   return (
     <div className="login-container">
@@ -16,14 +16,27 @@ export default function Login(props) {
         <div class="imagem-conector">
           <img
             className="image-login"
-            src="https://raw.githubusercontent.com/JoaoMWatson/fisco-conector/92c39b0104b9786ef6a38c814d3b970ff203ec88/src/images/API%20e%20Softwares%20-%20Copia.png"
+            src={require("../../images/image-login.png")}
           />
         </div>
         <div class="login-form">
-          <h1 className="login-title">Login</h1>
-          <LoginForm setCallbackAuth={setCallbackAuth}/>
+          <h1 className="login-title">- Login -</h1>
+          <LoginForm setCallbackAuth={setCallbackAuth} />
         </div>
         <div className="text-area-login">
+          <h1 className="login-title about-login">- Sobre -</h1>
+          <p className="paragrafer-login">
+            A Fiscosys é uma empresa especializada em soluções fiscais,
+            fornecendo soluções inteligentes, com profissionais altamente
+            qualificados, garantindo máxima qualidade na prestação de serviços
+            aos seus clientes e parceiros. Prezamos pela ética, honestidade e
+            transparência no trato com seus colaboradores, parceiros e clientes.
+            Nossa experiência está suportada na expertise legal e tributária de
+            nossa equipe de profissionais e no uso intensivo de tecnologia,
+            contando também com vivência em grandes corporações e nos principais
+            ERP`s tais como, SAP, Microsoft Dynamics AX, BAAN, Oracle, entre
+            outros.
+          </p>
           
         </div>
       </div>
